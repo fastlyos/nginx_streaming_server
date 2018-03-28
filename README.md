4363eb27799497f1dad1d3f3b2d70039c1f9a6a2
VIncent Cheng <vincentcheng62@gmail.com>
2018-03-28 11:07:20 +0800

    Update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-nginx
+Running environment: Ubuntu 16.04 server LTS
+
+Need to install nginx manually with the rtmp module
+
+Additional tools:
+-ffmpeg3
+
+$ sudo add-apt-repository ppa:jonathonf/ffmpeg-3
+$ sudo apt update
+$ sudo apt install ffmpeg libav-tools x264 x265
+
+-wondershaper
+-pcre
+-openssl
--- nginx_buildscript.sh
+++ nginx_buildscript.sh
@@ -1,6 +1,12 @@
 Can get previous one from
 $ sudo nginx -V
 
+PCRE library
+sudo apt-get install libpcre3 libpcre3-dev
+
+openssl
+sudo apt-get install openssl libssl-dev
+
 
 $ ./auto/configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/run/nginx.pid --lock-path=/var/lock/nginx.lock --user=www-data --group=www-data --build=Ubuntu --with-http_ssl_module --with-stream --with-mail=dynamic --with-http_mp4_module --with-http_flv_module --with-http_stub_status_module --add-module=/home/hmcheng/nginx/nginx-rtmp-module-master --add-module=/home/hmcheng/nginx/nginx-module-vts-master
 
