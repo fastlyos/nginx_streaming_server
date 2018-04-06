14a07ae487086ba12fed8e631995693bea9134f4
VIncent Cheng <vincentcheng62@gmail.com>
2018-04-06 15:14:48 +0800

    add echo-nginx-module

--- nginx_buildscript.sh
+++ nginx_buildscript.sh
@@ -8,7 +8,7 @@ openssl
 sudo apt-get install openssl libssl-dev
 
 
-$ ./auto/configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/run/nginx.pid --lock-path=/var/lock/nginx.lock --user=www-data --group=www-data --build=Ubuntu --with-http_ssl_module --with-stream --with-mail=dynamic --with-http_mp4_module --with-http_flv_module --with-http_stub_status_module --add-module=/home/hmcheng/nginx/nginx-rtmp-module-master --add-module=/home/hmcheng/nginx/nginx-module-vts-master
+$ ./auto/configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/run/nginx.pid --lock-path=/var/lock/nginx.lock --user=www-data --group=www-data --build=Ubuntu --with-http_ssl_module --with-stream --with-mail=dynamic --with-http_mp4_module --with-http_flv_module --with-http_stub_status_module --add-module=/home/hmcheng/nginx/nginx-rtmp-module-master --add-module=/home/hmcheng/nginx/nginx-module-vts-master  --add-module=/home/hmcheng/nginx/echo-nginx-module
 
 make -j 4
 sudo make install
