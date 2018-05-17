cba1950d7d8fe474671f4c530b33384c66325005
VIncent Cheng <vincentcheng62@gmail.com>
2018-05-17 09:54:51 +0800

    Update stream_localvideo_script.sh
    missing -re will make stat.xsl 'src' and 'low' video 0kbs all the time, that means those .ts has zero file size

--- stream_localvideo_script.sh
+++ stream_localvideo_script.sh
@@ -1,1 +1,1 @@
-ffmpeg -i ~/video_sample/sample10.mp4 -c:a aac -strict -2 -c:v libx264 -f flv rtmp://localhost/live/zzz
+ffmpeg -re -i ~/video_sample/sample10.mp4 -c:a aac -strict -2 -c:v libx264 -f flv rtmp://localhost/live/zzz
