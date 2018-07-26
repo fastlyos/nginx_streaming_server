195878a43a13c5e871d48bd482800c8ccbc9b180
VIncent Cheng <vincentcheng62@gmail.com>
2018-07-26 14:37:11 +0800

    Add files via upload

---
+++ openface_src_20180726_training_cpunet/opencv-gpu-py/src/face_detector.hpp
@@ -0,0 +1,32 @@
+#ifndef FACEDETECTOR_HPP
+#define FACEDETECTOR_HPP
+
+#include "opencv_version.hpp"
+
+#ifdef OPENCV_2
+  #include <opencv2/core/core.hpp>           // Rect
+  #include <opencv2/gpu/gpu.hpp>             // gpu::CascadeClassifier_GPU
+  #include <opencv2/objdetect/objdetect.hpp> // CascadeClassifier
+  #include "opencv2/opencv.hpp"
+#else
+  #include <opencv2/core.hpp>
+  #include <opencv2/objdetect.hpp>           // CascadeClassifier
+  #include <opencv2/cudaobjdetect.hpp>       // cuda::CascadeClassifier
+  #include <opencv2/videoio.hpp>
+#endif
+
+
+class FaceDetector
+{
+ public:
+  FaceDetector();
+  void Init(const std::string& cascade_file, bool gpu);
+  bool Detect(const char* image_file, std::vector<cv::Rect>& face_rects);
+ private:
+  bool gpu_;
+  std::string cascade_file_;
+  cv::VideoCapture vcap;
+};
+
+
+#endif
