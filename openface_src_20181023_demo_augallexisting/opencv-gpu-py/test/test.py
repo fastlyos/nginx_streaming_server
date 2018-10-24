04d72a58cacf45a582c4d1693fb6cd09f699abf2
VIncent Cheng <vincentcheng62@gmail.com>
2018-10-24 09:34:47 +0800

    Add files via upload

---
+++ openface_src_20181023_demo_augallexisting/opencv-gpu-py/test/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import cv2
+import cv2gpu
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+image_file = os.path.join(this_dir, 'family.jpg')
+
+cascade_file_cpu = os.path.join(this_dir, 'haarcascade_frontalface_default.xml')
+cascade_file_gpu = os.path.join(this_dir, 'haarcascade_frontalface_default_cuda.xml')
+
+def main():
+
+    if cv2gpu.is_cuda_compatible():
+        cv2gpu.init_gpu_detector(cascade_file_gpu)
+    else:
+        cv2gpu.init_cpu_detector(cascade_file_cpu)
+    
+    faces = cv2gpu.find_faces(image_file)
+    image = cv2.imread(image_file)
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #cv2.imshow('faces', image)
+    
+    cv2.imwrite('zzztest.jpg', image)
+
+if __name__ == '__main__':
+    main()
